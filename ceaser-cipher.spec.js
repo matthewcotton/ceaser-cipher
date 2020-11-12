9e6b5174558e96f2e320627b1448c5b0e12e6a20
matthewcotton <matthewneilcotton@gmail.com>
2020-11-12 09:41:54 -0500

    remove jest

--- ceaser-cipher.spec.js
+++
@@ -1,8 +0,0 @@
-import { ceaserCipher } from "./ceaser-cipher"
-
-describe("Ceaser Cipher", () => {
-    it("accepts text input from user", () => {
-        const result = ceaserCipher("test user input");
-        expect(result).toBe("test user input");
-    });
-})
